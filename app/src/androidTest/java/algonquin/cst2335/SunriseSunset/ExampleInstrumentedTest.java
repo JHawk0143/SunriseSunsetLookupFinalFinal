c33cbff11e625eb5d95ca45d23619d712faad971
Julianna Hawkins <hawk0143@algonquinlive.com>
2023-12-06 14:40:38 -0500

    Initial commit

---
+++ app/src/androidTest/java/algonquin/cst2335/SunriseSunset/ExampleInstrumentedTest.java
@@ -0,0 +1,29 @@
+package algonquin.cst2335.SunriseSunset;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    /**
+     * Test to ensure that the application's context has the correct package name
+     */
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("algonquin.cst2335.SunriseSunset", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/algonquin/cst2335/SunriseSunset/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package algonquin.cst2335.SunriseSunset;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    /**
+     * Test case to verify that addition is correct
+     */
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
